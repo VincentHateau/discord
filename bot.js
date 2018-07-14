3811a702642b170f57608969793fee13f3334717
HATEAU Vincent <bobvince_yeh@hotmail.fr>
2018-07-14 12:32:20 +0200

    Initialisation du répo

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+var auth = require('./auth.json');
+
+bot.on('ready', function () {
+  console.log("Je suis connecté !")
+})
+
+bot.on('message', message => {
+    if (message.content === 'ping') {
+      message.reply('pong !')
+    }
+  })
+
+bot.login(auth.token)
